cbf6a716a22f2e9fbb26df2a666f91d46075ce5b
Andy Tran <andy8tran@gmail.com>
2017-08-08 13:34:29 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+# good tip for 55 dollar meal
+mealcost = 55.00
+puts "A good tip for a $#{mealcost} meal is $#{mealcost*0.20}."
+
+# string add integer
+puts 1341234234.to_s + "string"
+
+# string interpolation
+puts "45628 * 7839 = #{45628 * 7839}"
+
+# logic practice
+puts (10 < 20 && 30 < 20) || !(10 == 11)
